6a91c5228cc5ef448280eaf1c1585496f3acce28
张凯文 <2021010729@secoder.net>
2024-08-15 11:29:14 +0800

    [A1T1] Finish Transform

--- src/scene/transform.cpp
+++ src/scene/transform.cpp
@@ -12,15 +12,25 @@ Mat4 Transform::parent_to_local() const {
 Mat4 Transform::local_to_world() const {
 	// A1T1: local_to_world
 	//don't use Mat4::inverse() in your code.
-
-	return Mat4::I; //<-- wrong, but here so code will compile
+	Mat4 r;
+	const Transform* cur = this;
+	while (std::shared_ptr <Transform> parent_ = cur->parent.lock()) {
+		r = cur->local_to_parent() * r;
+		cur = parent_.get();
+	}
+	return cur->local_to_parent() * r;
 }
 
 Mat4 Transform::world_to_local() const {
 	// A1T1: world_to_local
 	//don't use Mat4::inverse() in your code.
-
-	return Mat4::I; //<-- wrong, but here so code will compile
+	Mat4 r;
+	const Transform* cur = this;
+	while (std::shared_ptr <Transform> parent_ = cur->parent.lock()) {
+		r *= cur->parent_to_local();
+		cur = parent_.get();
+	}
+	return r * cur->parent_to_local();
 }
 
 bool operator!=(const Transform& a, const Transform& b) {
